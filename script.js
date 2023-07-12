88a96713760a2658a12da537f0c04bd2c71a02ac
Shailendra Sonar <77749654+ShailendraSonar@users.noreply.github.com>
2023-07-12 18:47:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+let youVariable = [
+	{
+		one :"1",
+		two : 2,
+		three: true
+	},
+	{
+		one :"3",
+		two : 4,
+		three: true
+	},
+	{
+		one :"5",
+		two : 6,
+		three: true
+	},
+	{
+		one :"7",
+		two : 8,
+		three: false
+	},
+	{
+		one :"9",
+		two : 10,
+		three: true
+	},
+]
